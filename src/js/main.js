db210937b2a9140811de04dcc88989910ce842a1
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-07-12 19:03:08 +0300

    fin sideNav design

---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+/// <reference types="../@types/jquery" />
+"use strict"
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{html,js}" ],
+  theme: {
+    extend: {
+      colors: {
+        "main-black": "#0D0D0D",
+        "main-white": "#f9f6f6",
+      }
+    },
+  },
+  plugins: [],
+}
+
