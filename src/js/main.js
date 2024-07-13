884d5065d25642d5fa20a9b293398d572f00c111
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-07-13 23:23:40 +0300

    fin sidenav main animation design

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,19 @@
 /// <reference types="../@types/jquery" />
 "use strict"
+let isOpened = true;
+function sideNavBarAction () {
+	$( '#innerNavBar' ).animate( { width: 'toggle' }, 500 );
+	isOpened = !isOpened;
+};
+sideNavBarAction();
+$( '#openCloseBtn' ).on( 'click', function () {
+	if ( !isOpened ) {
+		this.classList.remove( "fa-bars" );
+		this.classList.add( "fa-x" );
+	}
+	else {
+		this.classList.remove( "fa-x" );
+		this.classList.add( "fa-bars" );
+	}
+	sideNavBarAction();
+} )
